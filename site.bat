b8312bcdbdbab843c0fdc896a2378f681230fe4e
Sean Reilly <qwv4851@gmail.com>
2013-09-23 21:22:15 -0700

    Faster stream config

--- site.bat
+++ site.bat
@@ -1,2 +1,3 @@
 start node site/server.js
-start ffmpeg -f dshow -i video="Dxtory Video 1":audio="Stereo Mix (Realtek High Defini" -pix_fmt yuv420p -c:v libx264 -an -preset ultrafast -tune zerolatency -fflags nobuffer -f flv "rtmp://localhost:1935/live/test"
+ffmpeg -f dshow -i video="Dxtory
+Video 1" -r 25 -fflags nobuffer -threads 0 -f flv "rtmp://localhost:1935/live/test"
